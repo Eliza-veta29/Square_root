51fbcfab01449c8a926c5278f3b50397e4729e21
Eliza-veta29 <107852965+Eliza-veta29@users.noreply.github.com>
2022-07-20 23:10:04 +0300

    Update SQRServiceTest.java

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -2,6 +2,7 @@ package ru.netology.sqr;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.api.Assertions;
 
 public class SQRServiceTest {
 
@@ -10,7 +11,6 @@ public class SQRServiceTest {
     public void testSQRTInARange(int expected, int min, int max) {
         SQRService service = new SQRService();
         int actual = service.calcSqrtInRange(min, max);
-
+        Assertions.assertEquals(expected, actual);
     }
-
 }
